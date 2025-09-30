3725ee7a192a9aaf9ea33e877124c2ddbc8cf74f
Lei Wang <63298938+leiwang198506@users.noreply.github.com>
2025-09-30 15:32:21 -0400

    Initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,12 @@
+# Create a greeting for your program.
+# Ask the user for the city that they grew up in and store it in a variable.
+# Ask the user for the name of a pet and store it in a variable.
+# Combine the name of their city and pet and show them their band name.
+
+
+print("Welcome to the Band Name Generator.")
+city=input("What's the name of the city you grew up in?\n")
+petname=input("What is your pet name?\n")
+print("Your band name could be " + city+ " " + petname)
+
+
