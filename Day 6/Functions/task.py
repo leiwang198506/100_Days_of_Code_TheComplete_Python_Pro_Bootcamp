539743fe3905414570638359ad687b751218d9a2
Lei Wang <63298938+leiwang198506@users.noreply.github.com>
2025-10-16 14:34:01 -0400

    updating the data

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,21 @@
+#build in function
+#https://docs.python.org/3/library/functions.html
+print("Hello,what's up?")
+num_char=len("Hello,what's up?")
+print(num_char)
+
+#creat your own function,noting will happen.
+def my_function():
+    print("Hello,what's up?")
+    print("Bye!")
+
+#call the function
+my_function()
+
+
+  print("hahhaha")
+
+i = 1;
+while (i <= 10) {
+    echo i++;
+}
