9277d68f33b78cb7fbf0b7f592a24fa41cfd206a
Lei Wang <63298938+leiwang198506@users.noreply.github.com>
2025-11-13 14:33:58 -0500

    updating the data

--- Day 8/Caesar Cipher 2/main.py
+++ Day 8/Caesar Cipher 2/main.py
@@ -1,7 +1,4 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-
-
 # TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
 # TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
 #  by the shift amount and print the decrypted text.
--- Day 8/Caesar Cipher 3/solution.py
+++ Day 8/Caesar Cipher 3/solution.py
@@ -23,9 +23,7 @@ def caesar(original_text, shift_amount, encode_or_decode):
 
 
 should_continue = True
-
 while should_continue:
-
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
