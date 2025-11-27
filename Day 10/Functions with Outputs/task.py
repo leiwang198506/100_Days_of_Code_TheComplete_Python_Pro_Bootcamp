eee4cf49dfa5357e5c526d5a5cf1cfcffda9c524
Lei Wang <63298938+leiwang198506@users.noreply.github.com>
2025-11-27 15:04:12 -0500

    updating the data

--- Day 10/Functions with Outputs/task.py
+++ Day 10/Functions with Outputs/task.py
@@ -1,6 +1,15 @@
 def format_name(f_name, l_name):
+    """
+    This is the documentation,hahaha.
+    :param f_name: first names
+    :param l_name: last names
+    :return: the title case version of the names
+    """
+
     f_name_title=f_name.title()
     l_name_title =l_name.title()
+    # print(f"{f_name_title} {l_name_title}")
     return f"{f_name_title} {l_name_title}"
 
-format_name("leI","wANG")
+print(format_name("leI","wANG"))
+#
