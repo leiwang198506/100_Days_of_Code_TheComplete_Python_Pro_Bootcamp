407d624c54588bbe5deb33dc289cbf29492150c1
Lei Wang <63298938+leiwang198506@users.noreply.github.com>
2025-12-01 15:28:23 -0500

    updating the data

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,7 @@
 #add functions into the dictionary, with key = +-*/
+from operator import truediv
+
+
 def add(n1, n2):
     return n1 + n2
 def subtract(n1, n2):
@@ -13,38 +16,38 @@ operations = {
     "*": multiply,
     "/": divide
 }
-# Program asks the user to type the first number.
-# Program works out the result based on the chosen mathematical operator.
-def calculator(n1,n2):
-    result=operations[operation_choice](float(n1),float(n2))
-    return result
+logo = r'''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
+def calculator():
+    print(logo)
+    continue_calculate= True
+    n1 = float(input("What's the first number?:"))
+    while continue_calculate:
+        for key in operations:
+            print(key)
+        operation_choice = input("Pick an operation:")
+        n2 = float(input("What's the next number?:"))
+        result=operations[operation_choice](n1,n2)
+        print(f"{n1} {operation_choice} {n2} = {result}")
+        choice = input(
+            f"Type 'y' to continue calculating with {result} or type 'n' to start a new calculation:")
+        if choice == "y":
+            first_number= result
+        elif choice == "n":
+            print("\n"*100)
+            calculator()
+        else:
+            continue_calculate= False
 
-# Program asks if the user wants to continue working with the previous result.
-# If yes, program loops to use the previous result as the first number and then repeats the calculation process.
-# Program asks the user to type a mathematical operator (a choice of "+", "-", "*" or "/")
-first_number = input("What's the first number?:")
-for key in operations:
-    print(key)
-operation_choice = input("Pick an operation:")
-# Program asks the user to type the second number.
-second_number = input("What's the next number?:")
-output= calculator(first_number, second_number)
-print(f"{float(first_number)} {operation_choice} {float(second_number)} = {output}")
-should_continue = input(f"Type 'y' to continue calculating with {calculator(first_number, second_number)} or type 'n' to start a new calculation:")
-while should_continue == "y":
-    first_number= output
-    for key in operations:
-        print(key)
-    operation_choice = input("Pick an operation:")
-    second_number = input("What's the next number?:")
-    output=calculator(first_number, second_number)
-    print(f"{float(first_number)} {operation_choice} {float(second_number)} = {output}")
-    if should_continue == "n":
-        print("/n"*100)
-        calculator(first_number, second_number)
-
-
-
-
-    # If no, program asks the user for the fist number again and wipes all memory of previous calculations.
-# Add the logo from art.py
+calculator()
